3e48ef4c1d06b3fb38a2a22369724c9cbb5b6c88
Koji Saiki <saikou9901@gmail.com>
2019-09-01 17:54:20 +0900

    for push to github

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"io"
+	"log"
 	"net/http"
+	"os"
 
 	_ "./statik"
-
 	"github.com/rakyll/statik/fs"
 )
 
 func main() {
-	statikFS, _ := fs.New()
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := statikFS.Open("/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	io.Copy(os.Stdout, f)
+
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(statikFS)))
 	http.ListenAndServe(":8080", nil)
 }
